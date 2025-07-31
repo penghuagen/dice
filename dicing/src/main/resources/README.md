c87a3f1cb26974b56ec7b04b77e84598f565d341
penghg <maasplus@1234!>
2025-07-31 14:32:41 +0800

    setup(dice): 配置项目启动参数和日志路径
    - 修改正式环境和测试环境的数据库连接信息
    - 更新博饼开始时间和奖品信息
    - 添加用户信息
    - 设置日志文件路径
    - 更新项目启动命令

--- dicing/src/main/resources/README.md
+++ dicing/src/main/resources/README.md
@@ -3,7 +3,13 @@
 
 ## 启动
 - 正式
-nohup java -jar dicing.jar --spring.profiles.active=prod
+nohup java -jar dicing.jar --spring.profiles.active=prod\
+
+nohup java -jar dicing.jar --spring.profiles.active=prod > /dev/null 2>&1 &
 
 - 测试
 nohup java -jar dicing.jar --spring.profiles.active=test
+
+## 1、连接服务47.98.36.225
+## 2、执行初始化脚本(注意博饼时间，真实博饼时间以数据库里的为准)
+## 3、重启服务
--- dicing/src/main/resources/sql/dicing.sql
+++ dicing/src/main/resources/sql/dicing.sql
@@ -6,16 +6,16 @@ select * from user order by deskNo asc;
 
 -- 桌子信息
 delete from desk;
-insert into `desk`(`deskNo`,`state`,`roundNo`,`curSeqNo`,`noRemainingRoundNo`,`startTime`) values(1,0,1,0,-1,'2021-09-15 16:45:26.0');
+insert into `desk`(`deskNo`,`state`,`roundNo`,`curSeqNo`,`noRemainingRoundNo`,`startTime`) values(1,0,1,1,-1,'2025-07-31 14:40:00.0');
 
 -- 每桌奖品信息
 delete from deskaward;
-insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(2,0,32,1);
-insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(3,0,16,1);
-insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(4,0,8,1);
-insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(5,0,4,1);
-insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(6,0,2,1);
-insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(7,0,1,1);
+insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(2,32,32,1);
+insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(3,16,16,1);
+insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(4,8,8,1);
+insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(5,4,4,1);
+insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(6,2,2,1);
+insert into `deskaward`(`awardId`,`counter`,`remaining`,`deskNo`) values(7,1,1,1);
 
 -- 删除博饼日志
 delete from dicinglog;
@@ -23,5 +23,9 @@ delete from dicinglog;
 -- 用户信息
 delete from user;
 -- 第1桌
-insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(2700,'13800000001',1,1,0,'测试1','https://hwcimg2.fcxms.com/b2fc8d8e1c2d6520e3e111fb8b8ec16d.jpg',null);
-insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(1,'13800000002',1,2,0,'测试2','https://hwcimg2.fcxms.com/9ff259366be9f3da43c47dc42470684f.jpg',null);
+insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(21281367,'13720892285',1,1,0,'刘国巍','https://hwcimg2.fcxms.com/b2fc8d8e1c2d6520e3e111fb8b8ec16d.jpg',null);
+insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(21281363,'13850083825',1,2,0,'蔡天从','https://hwcimg2.fcxms.com/9ff259366be9f3da43c47dc42470684f.jpg',null);
+insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(21281368,'15280010342',1,3,0,'郑秀平','https://hwcimg2.fcxms.com/9ff259366be9f3da43c47dc42470684f.jpg',null);
+insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(21281370,'17326503007',1,4,0,'汪金杨','https://hwcimg2.fcxms.com/9ff259366be9f3da43c47dc42470684f.jpg',null);
+insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(21281369,'18050083886',1,5,0,'秦刚','https://hwcimg2.fcxms.com/9ff259366be9f3da43c47dc42470684f.jpg',null);
+insert into `user`(`userId`,`phone`,`deskNo`,`seqNo`,`ready`,`userName`,`avator`,`orgName`) values(779504,'18959278172',1,6,0,'彭华根','https://hwcimg2.fcxms.com/9ff259366be9f3da43c47dc42470684f.jpg',null);
